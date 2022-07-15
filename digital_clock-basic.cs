e2dd0f8e9202a605e835100d07ac6f614d15ee08
Sam <samael039@gmail.com>
2022-07-15 15:38:25 -0500

    commit on main

---
+++ digital_clock-basic.cs
@@ -0,0 +1,11 @@
+// Source: https://dyclassroom.com/csharp-project/how-to-create-a-digital-clock-in-csharp-using-visual-studio
+
+//Below creates an object named "Timer t" and sets the interval to 1 second which == 1000 ms.
+Timer t = new Timer();
+t.Interval = 1000;
+
+//Below is the event handler which will execute "some code" every second. Below will hold the code for the digital clock.
+private void t_Tick(object sender, EventArgs e)
+{
+
+}
